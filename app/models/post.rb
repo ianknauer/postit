fc8b20933c08d2eeb7a72fe3c154b29fb2eb8df0
ianknauer <ian.knauer@gmail.com>
2013-11-22 13:27:26 -0800

    extracted vote to module -> gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,27 +1,16 @@
 class Post < ActiveRecord::Base
+  include VoteableIankNov
+  
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
   
   before_save :generate_slug
   
   validates :title, presence: true, length: {minimum: 5}
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
-  
-  def total_votes
-    up_votes - down_votes
-  end
-  
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-  
-  def down_votes
-    self.votes.where(vote: false).size
-  end
   
   def generate_slug
     self.slug = self.title.gsub(' ', '-').downcase
